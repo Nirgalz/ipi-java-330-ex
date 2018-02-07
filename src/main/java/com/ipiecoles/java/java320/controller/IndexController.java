8310f6a24cc7bffb61ac2bae35cb29b20f0377b4
HEL <nicolas.hel@cgi.com>
2018-02-07 13:02:21 +0100

    g

--- src/main/java/com/ipiecoles/java/java320/controller/IndexController.java
+++ src/main/java/com/ipiecoles/java/java320/controller/IndexController.java
@@ -1,14 +1,23 @@
 package com.ipiecoles.java.java320.controller;
 
+import com.ipiecoles.java.java320.service.EmployeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Map;
+
 @Controller
 public class IndexController {
 
+    @Autowired
+    private EmployeService employeService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(){
+    public String index(Map<String, Object> model){
+        Long nbEmployes = employeService.countAllEmploye();
+        model.put("nbEmployes", nbEmployes);
         return "index";
     }
 
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -3,7 +3,7 @@
 
 <div class="container">
     <div class="jumbotron">
-        <h1>Bienvenue dans l'interface de gestion des <span></span> employés !</h1>
+        <h1>Bienvenue dans l'interface de gestion des <span>${nbEmployes}</span> employés !</h1>
         <p>Cette application web est paramétrée pour communiquer avec une API REST accessible à l'adresse <code>http://localhost:5367</code>.</p>
         <p>Il est nécessaire de développer les services webs nécessaires pour que cette application fonctionne. Voici l'ensemble des fonctionnalités :</p>
         <ul class="list-group">
